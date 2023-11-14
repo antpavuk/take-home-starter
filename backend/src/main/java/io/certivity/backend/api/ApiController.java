612ae06a6c82f3cc2e84a5438ec08d650ef0a450
Patrick Mularczyk <114990627+patrick-mularczyk@users.noreply.github.com>
2023-11-14 09:48:36 +0100

    add mongo config (#2)

--- backend/src/main/java/io/certivity/backend/api/ApiController.java
+++ backend/src/main/java/io/certivity/backend/api/ApiController.java
@@ -1,4 +1,4 @@
-package io.certivity.backend;
+package io.certivity.backend.api;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
--- backend/src/main/java/io/certivity/backend/config/CorsConfig.java
+++ backend/src/main/java/io/certivity/backend/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package io.certivity.backend;
+package io.certivity.backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
